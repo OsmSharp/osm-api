f276022eef0254fa29fc1a507788f1b56a0be000
xivk <ben@osmsharp.com>
2016-02-22 11:28:19 +0000

    Updated sharedassemblyversion.

--- OsmSharp.API.Tests/Properties/AssemblyInfo.cs
+++ OsmSharp.API.Tests/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("382b5e0a-3dd9-40c9-a65d-3df6e3450e06")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
